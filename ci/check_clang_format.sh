96e18d0901ad6053afddd42e738464971945888e
jeanmiii <valentin.dubos@epitech.eu>
2025-09-17 14:43:44 +0200

    [FIX] CI/CD clang-format

--- ci/check_clang_format.sh
+++ ci/check_clang_format.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-FILES=$(find src include -regex '.*\.\(cpp\|hpp\|h\)$')
-clang-format --dry-run --Werror $FILES
+DIRS=("Client" "Server" "libengine")
+
+for DIR in "${DIRS[@]}"; do
+    if [ -d "$DIR" ]; then
+        find "$DIR" -type f \( -name "*.cpp" -o -name "*.hpp" \) -exec clang-format -i {} +
+    fi
+done
+
+echo "✅ Clang format check passed."
